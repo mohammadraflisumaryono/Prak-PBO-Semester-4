fdfc8dde50fef07b769fd63ef7b9bb195a365c3d
Rizki Fahmi Sumaryono <mohammadraflisumaryono@gmail.com>
2024-03-09 20:33:40 +0700

    File 2C done :)

---
+++ Bitwise.java
@@ -0,0 +1,10 @@
+public class Bitwise {
+    public static void main(String[] args) {
+        int x = 5, y = 6;
+        System.out.println("X = " + x);
+        System.out.println("Y = " + y);
+        System.out.println("X & Y = " + (x & y));
+        System.out.println("X | Y = " + (x | y));
+        System.out.println("X ^ Y = " + (x ^ y));
+    }
+}
