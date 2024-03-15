b6526ad570192705d064d790872c76fc472d997c
Rizki Fahmi Sumaryono <mohammadraflisumaryono@gmail.com>
2024-03-15 13:53:22 +0700

    Modul 2

---
+++ Tugas 2/Modul3/Hewan.java
@@ -0,0 +1,33 @@
+public class Hewan {
+    String nama;
+    int berat;
+    int jumlahKaki;
+
+    public Hewan(String namaHewan, int beratHewan, int kakiHewan) {
+        nama = namaHewan;
+        berat = beratHewan;
+        jumlahKaki = kakiHewan;
+    }
+
+    public Hewan(String namaHewan) {
+        nama = namaHewan;
+    }
+
+    public Hewan() {
+
+    }
+
+    public void beratHewan(int beratHewan) {
+        berat = beratHewan;
+    }
+
+    public void jumlahKakiHewan(int jumlahKakiHewan) {
+        jumlahKaki = jumlahKakiHewan;
+    }
+
+    public void cetakHewan() {
+        System.out.println("Nama hewan : " + nama);
+        System.out.println("Berat hewan : " + berat + " kg");
+        System.out.println("Jumlah kaki: " + jumlahKaki);
+    }
+}
