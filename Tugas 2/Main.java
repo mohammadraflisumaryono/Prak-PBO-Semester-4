68f01e035b53a9c47a6d71c166bc8e88555dfe9b
Rizki Fahmi Sumaryono <mohammadraflisumaryono@gmail.com>
2024-03-15 13:43:17 +0700

    pertemuan 3

---
+++ Tugas 2/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Hewan elang = new Hewan();
+        elang.beratHewan(5);
+        elang.jumlahKakiHewan(2);
+        elang.cetakHewan();
+    }
+}
